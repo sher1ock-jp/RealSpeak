cc2f40aa7edaa764f082656103793d8760b07b11
sher1ock-jp <takaaaaadayo@gmail.com>
2025-04-28 23:16:15 +0900

    update output

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -2,13 +2,13 @@
 output "firebase_web_app_config" {
   description = "Firebase Web Appの設定情報"
   value = {
-    api_key             = google_firebase_web_app_config.default.api_key
+    api_key             = data.google_firebase_web_app_config.default.api_key
     auth_domain         = "${var.project_id}.firebaseapp.com"
     project_id          = var.project_id
     storage_bucket      = "${var.project_id}.appspot.com"
-    messaging_sender_id = google_firebase_web_app_config.default.messaging_sender_id
+    messaging_sender_id = data.google_firebase_web_app_config.default.messaging_sender_id
     app_id              = google_firebase_web_app.default.app_id
-    measurement_id      = google_firebase_web_app_config.default.measurement_id
+    measurement_id      = data.google_firebase_web_app_config.default.measurement_id
   }
   sensitive = true
 }
